417f856c762afb94ec947893c793e6bca464993d
Erwin Rooijakkers <erwin@rooijakkers.software>
2020-06-29 15:03:30 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nl.mediquest/logback.masking-pattern-layouts "1.0.5"
+(defproject nl.mediquest/logback.masking-pattern-layouts "1.0.6"
   :description "Logback appenders for scrubbing sensitive data from logs"
   :url "https://github.com/mediquest-nl/logback-masking-pattern-layouts"
   :license {:name "The MIT License"}
@@ -6,7 +6,7 @@
         nl.mediquest.logback.stackdriver-masking-pattern-layout]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.10.0"]
-                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]] ]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :test-selectors {:unit (complement :integration)
                    :integration :integration})
