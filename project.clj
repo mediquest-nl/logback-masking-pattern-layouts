57db0dbe5dc119d919947f58dd22266a24eedee5
Erwin Rooijakkers <erooijakkers@mediquest.nl>
2025-07-01 17:46:11 +0200

    Rename Stackdriver to Google Cloud Logging

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mediquest-nl/logback-masking-pattern-layouts"
   :license {:name "The MIT License"}
   :aot [nl.mediquest.logback.masking-pattern-layout
-        nl.mediquest.logback.stackdriver-masking-pattern-layout]
+        nl.mediquest.logback.google-cloud-masking-pattern-layout]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.10.0"]
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]]
